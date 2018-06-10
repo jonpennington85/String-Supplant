b7e6d0ad7e5daa2da1ea89eb86617da762b66e7a
Jonathan Pennington <jonathan.s.pennington@wmich.edu>
2018-06-10 01:43:46 -0400

    Changed storage of temp strings from heap to stack to reduce overhead.

--- strspplnt.c
+++ strspplnt.c
@@ -20,50 +20,47 @@
 char * strspplnt(char * string,char * toReplace,char * replaceWith){
 	char * spplntPtr=string;
 	char * cpPtr=string;
-	char * temp=malloc(strlen(string));
-	// Locate the pointer to the beginning of toReplace
+	char temp[strlen(string)];
+	// Move the pointer to the beginning of toReplace
 	while((spplntPtr=strstr(spplntPtr,toReplace))!=NULL){
-		// Move the copy pointer to right after what you want to replace
+		// Move the copy pointer to right after the substring we're going to replace
 		cpPtr=spplntPtr+strlen(toReplace);
 		// Copy cpPtr to temp
 		strcpy(temp,cpPtr);
-		// Add a null to the beginning of cpPtr
+		// Add a null to the beginning of spplntPtr
 		*spplntPtr='\0';
-		// Add replaceWith tospplntPtr
+		// Concatenate replaceWith to spplntPtr
 		strcat(spplntPtr,replaceWith);
 		spplntPtr+=strlen(replaceWith);
-		// Add rest of string afterwards
+		// Concatenate rest of string afterwards
 		strcat(spplntPtr,temp);
 	}
-	free(temp);
 	return string;
 }
 
 char * strnspplnt(char * string,char * toReplace,char * replaceWith,size_t maxlen){
-	char * temp=malloc(strnlen(string,maxlen)+1);
-	char * temp2=malloc(strnlen(string,maxlen)+1);
+	char temp[strnlen(string,maxlen)+1];
+	char temp2[strnlen(string,maxlen)+1];
 	char * spplntPtr=temp2;
 	char * cpPtr=temp2;
 	memset(temp,0,strnlen(string,maxlen)+1);
 	memset(temp2,0,strnlen(string,maxlen)+1);
 	strncpy(temp2,string,maxlen);
-	// Locate the pointer to the beginning of toReplace
+	// Move the pointer to the beginning of toReplace
 	while((spplntPtr=strstr(spplntPtr,toReplace))!=NULL){
-		// Move the copy pointer to right after what you want to replace
+		// Move the copy pointer to right after the substring we're going to replace
 		cpPtr=spplntPtr+strnlen(toReplace,maxlen);
 		// Copy cpPtr to temp
 		strncpy(temp,cpPtr,maxlen);
-		// Add a null to the beginning of cpPtr
+		// Add a null to the beginning of spplntPtr
 		*spplntPtr='\0';
-		// Add replaceWith tospplntPtr
+		// Concatenate replaceWith to spplntPtr
 		strncat(spplntPtr,replaceWith,maxlen-strnlen(temp2,maxlen));
 		spplntPtr+=strnlen(replaceWith,maxlen);
-		// Add rest of string afterwards
+		// Concatenate rest of string afterwards
 		strncat(spplntPtr,temp,maxlen-strnlen(temp2,maxlen));
 	}
 	memset(string,0,strlen(string));
 	strncpy(string,temp2,maxlen);
-	free(temp);
-	free(temp2);
 	return string;
 }
